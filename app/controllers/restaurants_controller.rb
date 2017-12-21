b8b60021ca0222120dd4a6685f51a05df215b24f
Ellen Lee <leechinghui.tw@gmail.com>
2017-12-21 13:33:02 +0800

    user can view recent restaurants and comments on feeds page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,4 +9,11 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @comment = Comment.new
   end
+
+  # GET restaurants/feeds
+  # 會去 render app/views/restuarants/feeds.html.erb
+  def feeds
+    @recent_restaurants = Restaurant.all.order(created_at: :desc).limit(10)
+    @recent_comments = Comment.all.order(created_at: :desc).limit(10)
+  end
 end
