089dd59b01e031e4f95f7dbd0dcea2931b6d45c8
Ellen Lee <leechinghui.tw@gmail.com>
2017-12-21 22:22:29 +0800

    setup relation among user, restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,10 @@ class Restaurant < ApplicationRecord
 
   # 當 Restaurant 物件被刪除時，順便刪除依賴的 Comment
   has_many :comments, dependent: :destroy
+
+  # 「餐廳被很多使用者收藏」的多對多關聯
+  # 將關聯名稱自訂為 :favorited_users
+  # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end
