07e45da698bb121ac5be418fc6636ca4ab1f6f01
Ellen Lee <leechinghui.tw@gmail.com>
2017-12-27 00:54:54 +0800

    setup routes for user index page and follow / unfollow function

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,8 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :followships, only: [:create, :destroy]
 
   resources :categories, only: :show
   root "restaurants#index"
